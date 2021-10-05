dee6941fc50b00e6b5ee2f568f862d251973313f
Alvin-n <nasibov.alvin@gmail.com>
2021-10-05 16:22:10 +0400

    home page updated

--- DailyShop/ecom_app/admin.py
+++ DailyShop/ecom_app/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
-from ecom_app.models import NavbarModel, Settings, Footer, ProductModel1, ProductModel2, Category,Sub_Category
+from ecom_app.models import NavbarModel, Settings, Footer, ProductModel, Category, Banner, MenProduct, WomenProduct,SportsProduct,ElectronicsProduct, RedCard, Latest_Blog, ClientBrand
 # Register your models here.
 
 admin.site.register(NavbarModel)
 admin.site.register(Settings)
 admin.site.register(Footer)
-admin.site.register(ProductModel1)
-admin.site.register(ProductModel2)
+admin.site.register(ProductModel)
 admin.site.register(Category)
-admin.site.register(Sub_Category)
+
+admin.site.register(Banner)
+admin.site.register(MenProduct)
+admin.site.register(WomenProduct)
+admin.site.register(SportsProduct)
+admin.site.register(ElectronicsProduct)
+admin.site.register(RedCard)
+admin.site.register(Latest_Blog)
+admin.site.register(ClientBrand)
