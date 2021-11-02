a692ee00a60e47cbc2e56ccdf995b5dd6b7935e5
Alvin-n <nasibov.alvin@gmail.com>
2021-11-02 16:44:50 +0400

    logout and updates

--- DailyShop/ecom_app/context_processors/context_processors.py
+++ DailyShop/ecom_app/context_processors/context_processors.py
@@ -1,22 +1,14 @@
-from ecom_app.models import NavbarModel, Settings, Footer, CategoryModel
+from ecom_app.models import Footer, NavbarModel, Settings
 
 def context(request):
-    context={}
+    context = {}
+    
     navbar_queryset = NavbarModel.objects.all()
-    settings_queryset = Settings.objects.all().first()
+    settings_queryset = Settings.objects.all()
     footer_queryset = Footer.objects.all()
-    # mininavbar_queryset = MiniNavbarModel.objects.all()
-    # redcard_queryset = RedCard.objects.all()
-    #product_queryset = ProductModel.objects.all()
-    category_queryset = CategoryModel.objects.all()
-    #subcategory_queryset = SubCategoryModel.objects.all()
 
+    context['footer_queryset'] = footer_queryset
     context['navbar_queryset'] = navbar_queryset
     context['settings_queryset'] = settings_queryset
-    context['footer_queryset'] = footer_queryset
-    #context['product'] = product_queryset
-    context['category_queryset'] = category_queryset
-    #context['subcategory_queryset'] = subcategory_queryset
 
-
-    return context #render(request, 'index.html', context)
+    return context
--- DailyShop/ecom_app/urls.py
+++ DailyShop/ecom_app/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path 
-
-from .views import home_view, contact_view,catalog_view, category_view, product_detail_view, product_view, account_view, logout_view
-
+from .views import  account_view, category_view, contact_view, home_view, logout_view, product_detail_view, product_view
 
 urlpatterns = [
     path('', home_view, name = 'home_page'),
     path('contact/', contact_view, name='contact_page'),
     path('category/<int:navbar_id>/', category_view, name='category_page'),
     path('product_detail/<int:product_id>/', product_detail_view, name='product_detail_page'),
-    path('product/<str:name>/', product_view, name="product_page"),
+    path('product/<int:name>/', product_view, name="product_page"),
     path('account/', account_view, name='account_page'),
-    path('logout/', logout_view, name='logout_page')
+     path('logout/', logout_view, name='logout_page')
 ]
