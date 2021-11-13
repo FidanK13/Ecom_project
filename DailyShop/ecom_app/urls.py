5969b06d4ac92f8cc84ab13ea6490a43a18ae8a2
Alvin-n <nasibov.alvin@gmail.com>
2021-11-13 15:27:49 +0400

    related_category_added

--- DailyShop/ecom_app/urls.py
+++ DailyShop/ecom_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path 
-from .views import  account_view, category_view, contact_view, home_view, logout_view, product_detail_view, product_view
+from .views import  account_view, category_view, contact_view, filtired_product_view, home_view, logout_view, product_detail_view, product_view
 
 urlpatterns = [
     path('', home_view, name = 'home_page'),
@@ -7,6 +7,7 @@ urlpatterns = [
     path('category/<int:navbar_id>/', category_view, name='category_page'),
     path('product_detail/<int:product_id>/', product_detail_view, name='product_detail_page'),
     path('product/<int:name>/', product_view, name="product_page"),
+    path('product/<int:name>/<int:catalog_id>/', filtired_product_view, name="filtired_product_page"),
     path('account/', account_view, name='account_page'),
-     path('logout/', logout_view, name='logout_page')
+    path('logout/', logout_view, name='logout_page')
 ]
